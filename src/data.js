ad73b4a66bb72a967acb713518c914c3eeed654e
titobzzz <olumoyinjoshua19609@gmail.com>
2022-08-28 14:14:18 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const data = [
+    {
+        id: 1,
+        body: `### `,
+    },
+    { id: 2, body: `a` },
+    { id: 3, body: `b` },
+    { id: 4, body: `c` },
+    { id: 5, body: `d` },
+    { id: 6, body: `e` },
+];
+
+export default data;
